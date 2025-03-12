cae05cdaaf257bd7fb4cbd0cc82d3e9f73ab7dcb
ShadowNinja <75316298+Sufyaan323@users.noreply.github.com>
2025-03-12 14:07:13 +0000

    Update crud.php

--- crud.php
+++ crud.php
@@ -4,7 +4,7 @@
 
     <title>Create</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="Crud-style.css">
+    <link rel="stylesheet" href="crudStyle.css">
 </head>
 <body>
     <div class="container">
