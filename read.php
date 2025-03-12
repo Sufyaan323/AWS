86954778e43919c8d12cefdb820e079bf730f6d3
ShadowNinja <75316298+Sufyaan323@users.noreply.github.com>
2025-03-12 14:10:15 +0000

    Update read.php

--- read.php
+++ read.php
@@ -11,7 +11,7 @@ $result = mysqli_query($conn, $sql);
 <head>
     <title>Read</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="Crud-style.css">
+    <link rel="stylesheet" href="crudStyle.css">
 </head>
 <body>
     <div class="container">
